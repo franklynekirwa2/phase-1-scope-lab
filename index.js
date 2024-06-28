33a6ce8f7244957c4e89ebd0830ef446efa32e8e
franklynekirwa2 <sangkirwa2@gmail.com>
2024-06-28 21:52:40 +0300

    addscope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName= 'bob';
+
+function upperCaseCustomerName() {
+
+ customerName=customerName.toUpperCase();
+
+}
+function  setBestCustomer(){
+    bestCustomer ='not bob';
+}
+
+function overwriteBestCustomer(){
+       bestCustomer='maybe bob';
+}
+
+ const LeastFavoriteCustomer="frank";
+ function changeLeastFavoriteCustomer(){
+            LeastFavoriteCustomer = 'kirwa';
+         }
+         try{
+            changeLeastFavoriteCustomer();
+           }
+          catch (error) {
+            console.error("Error:", message);
+        }
+
+
+
